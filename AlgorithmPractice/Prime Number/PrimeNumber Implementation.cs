55d0051940c42d88224ca35b956a0d11365252e8
Bismuth083 <147175162+Bismuth083@users.noreply.github.com>
2025-10-21 21:47:19 +0900

    素数判定アルゴリズムの追加

---
+++ AlgorithmPractice/Prime Number/PrimeNumber Implementation.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Math;
+
+namespace Bismuth083.AlgorithmPractice
+{
+  public static class PrimeNumber
+  {
+    public static bool IsPrime(this ulong num)
+    {
+      // 1以下の数は考慮しない
+      if (num < 2) return false;
+
+      // 2は素数
+      if (num == 2) return true;
+
+      // 2以外の偶数は素数ではない
+      if (num % 2 == 0) return false;
+
+
+      // 3以上の奇数について、全ての奇数で割り切れないか確認する
+      for (ulong i = 3; i <= Ceiling(Sqrt(num)); i += 2)
+      {
+        if (num % i == 0)
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    public static bool IsPrime(this long num)
+    {
+      if (num < 0) return false;
+      return IsPrime((ulong)num);
+    }
+
+    public static bool IsPrime(this int num)
+    {
+      return IsPrime((long)num);
+    }
+
+    public static List<int> GetPrimesUpTo(int limit)
+    {
+      // 2未満の場合は空リストを返す
+      if (limit < 2) return new List<int>();
+
+      // 0からlimitまでFalseで初期化
+      // 3以上の奇数のみTrueに書き換える
+      var primes = new bool[limit + 1];
+      primes[2] = true;
+      for (int i = 3; i <= limit; i += 2)
+      {
+        primes[i] = true;
+      }
+
+      // エラトステネスの篩(3以降の奇数のみ調べている)
+      int cntPrime = 1;
+      for (int i = 3; i <= Ceiling(Sqrt(limit)); i += 2)
+      {
+        if (primes[i])
+        {
+          cntPrime++;
+          for (int j = 2 * i; j <= limit; j += i) {
+            primes[j] = false;
+          }
+        }
+      }
+
+      // 素数リストの作成
+      List<int> result = new List<int>(cntPrime) {2};
+      for (int i = 3; i <= limit; i += 2)
+      {
+        if (primes[i])
+        {
+          result.Add(i);
+        }
+      }
+      return result;
+    }
+
+    public static List<ulong> PrimeFactorization(ulong num)
+    {
+      if (num == 0) throw new System.ArgumentException("Zero is not supported", nameof(num));
+      var factors = new List<ulong>();
+
+      while (true)
+      {
+        if (num % 2 == 0)
+        {
+          factors.Add(2);
+          num /= 2;
+          continue;
+        }
+        break;
+      }
+
+      for (ulong i = 3; i <= Ceiling(Sqrt(num)); i += 2)
+      {
+        while (true)
+        {
+          if (num % i == 0)
+          {
+            factors.Add(i);
+            num /= i;
+            continue;
+          }
+          break;
+        }
+      }
+      
+      if (num != 1) factors.Add(num);
+
+      return factors;
+    }
+
+    public static List<ulong> PrimeFactorization(long num)
+    {
+      num =  num < 0 ? Abs(num) : num;
+      return PrimeFactorization((ulong)num);
+    }
+  }
+}
+
+// https://github.com/Bismuth083/AlgorithmPractice
---
+++ Test/PrimeNumber.cs
@@ -0,0 +1,60 @@
+﻿namespace Bismuth083.AlgorithmPractice.Test;
+using Bismuth083.AlgorithmPractice;
+
+public class TestPrimeNumber
+{
+  [Test]
+  public void CheckIsPrime()
+  {
+    Assert.That(2.IsPrime(), Is.True);
+    Assert.That(3.IsPrime(), Is.True);
+    Assert.That(4.IsPrime(), Is.False);
+    Assert.That(13.IsPrime(), Is.True);
+    Assert.That(25.IsPrime(), Is.False);
+    Assert.That(37.IsPrime(), Is.True);
+    Assert.That(99.IsPrime(), Is.False);
+    Assert.That(100.IsPrime(), Is.False);
+    Assert.That(7919.IsPrime(), Is.True);
+    Assert.That(10007.IsPrime(), Is.True);
+    Assert.That(100003.IsPrime(), Is.True);
+    Assert.That(1000003.IsPrime(), Is.True);
+    Assert.That(10000019.IsPrime(), Is.True);
+    Assert.That(100000007.IsPrime(), Is.True);
+    Assert.That(167772161.IsPrime(), Is.True);
+    Assert.That(469762049.IsPrime(), Is.True);
+    Assert.That(924844033.IsPrime(), Is.True);
+    Assert.That(998244353.IsPrime(), Is.True);
+    Assert.That(1000000007.IsPrime(), Is.True);
+    Assert.That(1000000009.IsPrime(), Is.True);
+  }
+
+  [Test]
+  public void CheckGetPrimesUpTo()
+  {
+    CollectionAssert.AreEqual(new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 }, PrimeNumber.GetPrimesUpTo(31).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 }, PrimeNumber.GetPrimesUpTo(50).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199 }, PrimeNumber.GetPrimesUpTo(200).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2 }, PrimeNumber.GetPrimesUpTo(2).ToList());
+    CollectionAssert.AreEqual(new List<int> { }, PrimeNumber.GetPrimesUpTo(1).ToList());
+    CollectionAssert.AreEqual(new List<int> { }, PrimeNumber.GetPrimesUpTo(0).ToList());
+    CollectionAssert.AreEqual(new List<int> { }, PrimeNumber.GetPrimesUpTo(-10).ToList());
+  }
+
+  [Test]
+  public void CheckPrimeFactorization()
+  {
+    CollectionAssert.AreEqual(new List<int> { }, PrimeNumber.PrimeFactorization(1).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2 }, PrimeNumber.PrimeFactorization(2).ToList());
+    CollectionAssert.AreEqual(new List<int> { 3 }, PrimeNumber.PrimeFactorization(3).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2, 3 }, PrimeNumber.PrimeFactorization(6).ToList());
+    CollectionAssert.AreEqual(new List<int> { 3, 3 }, PrimeNumber.PrimeFactorization(9).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2, 2, 2, 3, 3, 5 }, PrimeNumber.PrimeFactorization(360).ToList());
+    CollectionAssert.AreEqual(new List<int> { 37 }, PrimeNumber.PrimeFactorization(37).ToList());
+    CollectionAssert.AreEqual(new List<int> { 3, 19 }, PrimeNumber.PrimeFactorization(57).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2, 2, 5, 5 }, PrimeNumber.PrimeFactorization(100).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2, 2, 3, 3, 277 }, PrimeNumber.PrimeFactorization(9972).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2, 3, 3, 5, 3607, 3803 }, PrimeNumber.PrimeFactorization(1234567890).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2, 1000000007 }, PrimeNumber.PrimeFactorization(2000000014).ToList());
+    CollectionAssert.AreEqual(new List<int> { 2, 2, 2, 3, 3, 5 }, PrimeNumber.PrimeFactorization(-360).ToList());
+  }
+}
