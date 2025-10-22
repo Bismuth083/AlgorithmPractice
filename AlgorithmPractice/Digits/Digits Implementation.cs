1222a89e2ec804b002b97f1aedc24d24ad3586f2
Bismuth083 <147175162+Bismuth083@users.noreply.github.com>
2025-10-22 23:02:10 +0900

    桁数を計算するアルゴリズムを実装

---
+++ AlgorithmPractice/Digits/Digits Implementation.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using static System.Math;
+
+namespace Bismuth083.AlgorithmPractice
+{
+  public static class Digits
+  {
+    public static int CalcDigits(ulong number)
+    {
+      if(number >= Dig11)
+      {
+        if (number >= Dig16)
+        {
+          if(number >= Dig19)
+          {
+            if(number >= Dig20)
+            {
+              return 20;
+            }
+            else
+            {
+              return 19;
+            }
+          }
+          else
+          {
+            if(number >= Dig18)
+            {
+              return 18;
+            }
+            else
+            {
+              if(number >= Dig17)
+              {
+                return 17;
+              }
+              else
+              {
+                return 16;
+              }
+            }
+          }
+        }
+        else
+        {
+          if(number >= Dig14)
+          {
+            if(number >= Dig15)
+            {
+              return 15;
+            }
+            else
+            {
+              return 14;
+            }
+          }
+          else
+          {
+            if(number >= Dig13)
+            {
+              return 13;
+            }
+            else
+            {
+              if(number >= Dig12)
+              {
+                return 12;
+              }
+              else
+              {
+                return 11;
+              }
+            }
+          }
+        }
+      }
+      else
+      {
+        if(number >= Dig06)
+        {
+          if (number >= Dig09)
+          {
+            if(number >= Dig10)
+            {
+              return 10;
+            }
+            else
+            {
+              return 9;
+            }
+          }
+          else
+          {
+            if(number >= Dig08)
+            {
+              return 8;
+            }
+            else
+            {
+              if(number >= Dig07)
+              {
+                return 7;
+              }
+              else
+              {
+                return 6;
+              }
+            }
+          }
+        }
+        else
+        {
+          if(number >= Dig04)
+          {
+            if(number >= Dig05)
+            {
+              return 5;
+            }
+            else
+            {
+              return 4;
+            }
+          }
+          else
+          {
+            if(number >= Dig03)
+            {
+              return 3;
+            }
+            else
+            {
+              if(number >= Dig02)
+              {
+                return 2;
+              }
+              else
+              {
+                return 1;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    public static int CalcDigits(long number) {
+      return CalcDigits((ulong)Abs(number));
+    }
+
+    const ulong Dig02 = 10UL;
+    const ulong Dig03 = 100UL;
+    const ulong Dig04 = 1000UL;
+    const ulong Dig05 = 10000UL;
+    const ulong Dig06 = 100000UL;
+    const ulong Dig07 = 1000000UL;
+    const ulong Dig08 = 10000000UL;
+    const ulong Dig09 = 100000000UL;
+    const ulong Dig10 = 1000000000UL;
+    const ulong Dig11 = 10000000000UL;
+    const ulong Dig12 = 100000000000UL;
+    const ulong Dig13 = 1000000000000UL;
+    const ulong Dig14 = 10000000000000UL;
+    const ulong Dig15 = 100000000000000UL;
+    const ulong Dig16 = 1000000000000000UL;
+    const ulong Dig17 = 10000000000000000UL;
+    const ulong Dig18 = 100000000000000000UL;
+    const ulong Dig19 = 1000000000000000000UL;
+    const ulong Dig20 = 10000000000000000000UL;
+  }
+}
+
+// https://github.com/Bismuth083/AlgorithmPractice
---
+++ Test/Digits.cs
@@ -0,0 +1,52 @@
+﻿using static Bismuth083.AlgorithmPractice.Digits;
+namespace Bismuth083.AlgorithmPractice.Test;
+
+public class TestDigits
+{
+  [Test]
+  public void TestCalcDigits()
+  {
+    Assert.That(CalcDigits(0), Is.EqualTo(1));
+    Assert.That(CalcDigits(1), Is.EqualTo(1));
+    Assert.That(CalcDigits(5), Is.EqualTo(1));
+    Assert.That(CalcDigits(9), Is.EqualTo(1));
+    Assert.That(CalcDigits(10), Is.EqualTo(2));
+    Assert.That(CalcDigits(99), Is.EqualTo(2));
+    Assert.That(CalcDigits(100), Is.EqualTo(3));
+    Assert.That(CalcDigits(999), Is.EqualTo(3));
+    Assert.That(CalcDigits(1_000), Is.EqualTo(4));
+    Assert.That(CalcDigits(9_999), Is.EqualTo(4));
+    Assert.That(CalcDigits(10_000), Is.EqualTo(5));
+    Assert.That(CalcDigits(99_999), Is.EqualTo(5));
+    Assert.That(CalcDigits(100_000), Is.EqualTo(6));
+    Assert.That(CalcDigits(999_999), Is.EqualTo(6));
+    Assert.That(CalcDigits(1_000_000), Is.EqualTo(7));
+    Assert.That(CalcDigits(9_999_999), Is.EqualTo(7));
+    Assert.That(CalcDigits(10_000_000), Is.EqualTo(8));
+    Assert.That(CalcDigits(99_999_999), Is.EqualTo(8));
+    Assert.That(CalcDigits(100_000_000), Is.EqualTo(9));
+    Assert.That(CalcDigits(999_999_999), Is.EqualTo(9));
+    Assert.That(CalcDigits(1_000_000_000), Is.EqualTo(10));
+    Assert.That(CalcDigits(9_999_999_999), Is.EqualTo(10));
+    Assert.That(CalcDigits(10_000_000_000), Is.EqualTo(11));
+    Assert.That(CalcDigits(99_999_999_999), Is.EqualTo(11));
+    Assert.That(CalcDigits(100_000_000_000), Is.EqualTo(12));
+    Assert.That(CalcDigits(999_999_999_999), Is.EqualTo(12));
+    Assert.That(CalcDigits(1_000_000_000_000), Is.EqualTo(13));
+    Assert.That(CalcDigits(9_999_999_999_999), Is.EqualTo(13));
+    Assert.That(CalcDigits(10_000_000_000_000), Is.EqualTo(14));
+    Assert.That(CalcDigits(99_999_999_999_999), Is.EqualTo(14));
+    Assert.That(CalcDigits(100_000_000_000_000), Is.EqualTo(15));
+    Assert.That(CalcDigits(999_999_999_999_999), Is.EqualTo(15));
+    Assert.That(CalcDigits(1_000_000_000_000_000), Is.EqualTo(16));
+    Assert.That(CalcDigits(9_999_999_999_999_999), Is.EqualTo(16));
+    Assert.That(CalcDigits(10_000_000_000_000_000), Is.EqualTo(17));
+    Assert.That(CalcDigits(99_999_999_999_999_999), Is.EqualTo(17));
+    Assert.That(CalcDigits(100_000_000_000_000_000), Is.EqualTo(18));
+    Assert.That(CalcDigits(999_999_999_999_999_999), Is.EqualTo(18));
+    Assert.That(CalcDigits(1_000_000_000_000_000_000), Is.EqualTo(19));
+    Assert.That(CalcDigits(9_999_999_999_999_999_999), Is.EqualTo(19));
+    Assert.That(CalcDigits(10_000_000_000_000_000_000), Is.EqualTo(20));
+    Assert.That(CalcDigits(UInt64.MaxValue), Is.EqualTo(20));
+  }
+}
